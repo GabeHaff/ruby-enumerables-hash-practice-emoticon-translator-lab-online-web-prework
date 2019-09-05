f1daca2b572e7146cc696c23bfffc14086954624
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 23:31:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ load_hash
 end 
 def get_japanese_emoticon(yaml_file,emoticon)
   library = load_library(yaml_file)
-  load_hash["get_emoticon"].each do |english, japanese|
+  load_hash{"get_emoticon"}.each do |english, japanese|
     if emoticon == english 
       load_hash = japanese
     end
