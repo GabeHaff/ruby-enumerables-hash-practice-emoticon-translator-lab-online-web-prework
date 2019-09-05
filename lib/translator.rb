5faf7733d8f2ff5d18f7486f12cb2e17a793e88f
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 23:43:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ load_hash
 end 
 def get_japanese_emoticon(yaml_file,emoticon)
   library = load_library(yaml_file)
-  library["get_emoticon"].each do |english, japanese|
+  load_hash["get_emoticon"].each do |english, japanese|
     if emoticon == english
     return "Sorry, that emoticon was not found"
   end
