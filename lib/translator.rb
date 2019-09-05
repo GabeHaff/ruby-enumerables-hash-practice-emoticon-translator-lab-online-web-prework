7faca17cec31cadd405258f11683151cd7d21c30
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 23:06:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,5 +3,5 @@ def load_library(file)
   library= YAML.load_file(file)
  library.each do |trans, emote|
   load_hash = {"get_meaning" => {} , "get_emoticon" =>{} }
-  load_hash["get_meaning"][emote]
+  load_hash["get_meaning"][emote[1]]
 end
