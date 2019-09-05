056cc4708924d7044cced42229793b91580a7566
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 22:00:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,5 @@
 require"yaml"
 def load_library("./lib/emoticons.yml")
-  load_hash = {get_meaning => {} 
+  load_hash = {"get_meaning" => {} , "get_emoticon" =>{}
 YAML.load_file("./lib/emoticons.yml")
 
