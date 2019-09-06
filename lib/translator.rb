c31ceae4360a2798cd9fd1a63711c9529d963687
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 00:27:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,5 +8,10 @@ def load_library(file)
 end
 load_hash
 end 
-def get_japanese_emoticon
-  
+def get_japanese_emoticon(file_path, emoticon)
+   library = load_library(file_path) 
+  result = library['get_emoticon'][emoticon]
+
+   result ? result : "Sorry, that emoticon was not found"
+end	
+end 
