ccf214bc77c741a2d37a0071da9112cc0a5c0a75
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 00:03:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def get_japanese_emoticon(file,emoticon)
       return japanese
   end
 end
-if emoticon == nil
+if emoticon[] == nil
   return "Sorry, that emoticon was not found"
 else 
   return emoticon
