5413c00c9c1ace6b7fc6e0c5e55a6ca827df68e9
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 00:19:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ end
 load_hash
 end 
 def get_japanese_emoticon(file_path,emoticon)
-  library = load_library(file)
+  library = load_library(file_p)
   library["get_emoticon"].each do |english,japanese|
     if result == english 
       return japanese
